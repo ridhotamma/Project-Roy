f51a72c273b092420b2da003e5d2ca817b1ed91c
Ridho Tamma <ridho.tamma@gmail.com>
2024-05-30 21:18:27 +0700

    remove encryption impl

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -1,6 +1,4 @@
-import jwt
 from passlib.context import CryptContext
-from app.config import SECRET_KEY, ALGORITHM
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -11,13 +9,3 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
-
-
-def encrypt_password(password: str) -> str:
-    encoded_jwt = jwt.encode({"password": password}, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-def decrypt_password(encoded_jwt: str) -> str:
-    decoded_jwt = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=[ALGORITHM])
-    return decoded_jwt["password"]
