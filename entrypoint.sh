f7d5df5753ec1bcbe9a789c7c441fbb6740b7940
Ridho Tamma <ridho.tamma@gmail.com>
2024-07-23 16:57:15 +0700

    fix several errors when running celery using docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start the Celery worker in the background
-celery -A tasks.celery_app worker --loglevel=info &
+celery -A app.tasks.celery_app worker --loglevel=info &
 
 # Start the Celery beat scheduler in the background
-celery -A tasks.celery_app beat --loglevel=info &
+celery -A app.tasks.celery_app beat --loglevel=info &
 
 # Start the FastAPI app in the background
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
